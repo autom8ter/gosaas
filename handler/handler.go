84de27b80d84afb0652f8642b85994dc70931f79
autom8ter <admin@autom8ter.com>
2019-04-30 14:56:19 -0600

    update

--- handler/handler.go
+++ handler/handler.go
@@ -2,12 +2,11 @@ package handler
 
 import (
 	"context"
-	"fmt"
 	"github.com/autom8ter/api"
 	"github.com/autom8ter/api/common"
 	"github.com/autom8ter/auth0/endpoints"
-	"github.com/autom8ter/objectify"
-	"github.com/gorilla/mux"
+	"github.com/autom8ter/gosaas/sessions"
+	"github.com/autom8ter/gosaas/util"
 	"html/template"
 	"io"
 	"io/ioutil"
@@ -16,21 +15,13 @@ import (
 	"strings"
 )
 
-var util = objectify.Default()
-
 type Handler struct {
 	*api.OAuth2
-	Port         int
-	APIAddr      string
-	Domain       string
-	HomePath     string
-	LoggedInPath string
-	LoginPath    string
-	LogoutPath   string
-	CallbackPath string
+	APIAddr string
+	Domain  string
 }
 
-func NewHandler(domain, clientID, clientSecret, redirect, homePath, loggedInPath, loginPath, logoutPath, callbackPath string) *Handler {
+func NewHandler(domain, clientID, clientSecret, redirect string) *Handler {
 
 	return &Handler{
 		OAuth2: &api.OAuth2{
@@ -41,16 +32,18 @@ func NewHandler(domain, clientID, clientSecret, redirect, homePath, loggedInPath
 			Scopes:       common.ToStringArray([]string{"openid", "profile", "email"}),
 			Redirect:     common.ToString(redirect),
 		},
-		HomePath:     homePath,
-		LoggedInPath: loggedInPath,
-		LoginPath:    loginPath,
-		LogoutPath:   logoutPath,
-		CallbackPath: callbackPath,
 	}
 }
 
-func (h *Handler) callbackHandler() http.HandlerFunc {
+func (h *Handler) Callback(loggedInPath string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		state := r.URL.Query().Get("state")
+
+		if state != sessions.State(r).Values["state"] {
+			http.Error(w, "Invalid state parameter", http.StatusInternalServerError)
+			return
+		}
+
 		h.Code = common.ToString(r.URL.Query().Get("code"))
 		t, err := h.Token()
 		if err != nil {
@@ -58,14 +51,6 @@ func (h *Handler) callbackHandler() http.HandlerFunc {
 			return
 		}
 
-		r.AddCookie(&http.Cookie{
-			Name:  "access_token",
-			Value: t.AccessToken,
-		})
-		r.AddCookie(&http.Cookie{
-			Name:  "id_token",
-			Value: string(util.MarshalJSON(t.Extra("id_token"))),
-		})
 		set, err := h.NewAPIClientSet(context.TODO(), h.APIAddr)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -80,17 +65,16 @@ func (h *Handler) callbackHandler() http.HandlerFunc {
 			http.Error(w, "user not found", http.StatusInternalServerError)
 			return
 		}
-		r.AddCookie(&http.Cookie{
-			Name:  "user",
-			Value: string(util.MarshalJSON(usr)),
-		})
-		http.Redirect(w, r, h.LoggedInPath, http.StatusSeeOther)
+		s := sessions.Auth(r)
+		s.Values["userinfo"] = util.Util.ToMap(usr)
+
+		if err := s.Save(r, w); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		http.Redirect(w, r, loggedInPath, http.StatusSeeOther)
 	}
 
-}
-
-func (a *Handler) ListenAndServe(home, loggedIn http.HandlerFunc) error {
-	return http.ListenAndServe(fmt.Sprintf(":%v", a.Port), a.Router(home, loggedIn))
 }
 
 func (c *Handler) logoutURL(returnTo string) (string, error) {
@@ -109,33 +93,23 @@ func (c *Handler) logoutURL(returnTo string) (string, error) {
 	return Url.String(), nil
 }
 
-func (a *Handler) Router(home, loggedIn http.HandlerFunc) *mux.Router {
-	m := mux.NewRouter()
-	m.HandleFunc(a.LogoutPath, a.Logout(fmt.Sprintf("http://localhost:%v", a.Port)))
-	m.HandleFunc(a.LoginPath, a.Login(""))
-	m.HandleFunc(a.CallbackPath, a.callbackHandler())
-	m.HandleFunc(a.HomePath, home)
-	m.Handle(a.LoggedInPath, a.RequireLogin(loggedIn))
-
-	return m
-}
-
-func (a *Handler) RequireLogin(next http.HandlerFunc) http.HandlerFunc {
+func (a *Handler) RequireLogin(redirect string, next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		usr, err := r.Cookie("user")
+		usr := sessions.Auth(r).Values["userinfo"]
+
 		if usr == nil {
-			http.Redirect(w, r, a.LoginPath, http.StatusSeeOther)
+			http.Redirect(w, r, redirect, http.StatusSeeOther)
 			return
 		}
 
 		u := &api.User{}
-		err = u.UnmarshalJSONFrom(util.MarshalJSON(usr.Value))
+		err := u.UnmarshalJSONFrom(util.Util.MarshalJSON(usr))
 		if err != nil {
-			http.Redirect(w, r, a.LoginPath, http.StatusSeeOther)
+			http.Redirect(w, r, redirect, http.StatusSeeOther)
 			return
 		}
 		if u == nil {
-			http.Redirect(w, r, a.LoginPath, http.StatusSeeOther)
+			http.Redirect(w, r, redirect, http.StatusSeeOther)
 			return
 		}
 		next(w, r)
@@ -155,19 +129,15 @@ func (a *Handler) Logout(returnTo string) http.HandlerFunc {
 
 func (a *Handler) Login(aud string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		if aud == "" {
-			aud = "https://" + a.Domain + "/userinfo"
-		}
-		state := util.RandomString(32)
-		r.AddCookie(&http.Cookie{
-			Name:  "state",
-			Value: state,
-		})
-		http.Redirect(w, r, a.AuthCodeURL(state, aud), http.StatusTemporaryRedirect)
+		state := util.Util.RandomString(32)
+		s := sessions.State(r)
+		s.Values["state"] = state
+		_ = s.Save(r, w)
+		http.Redirect(w, r, a.AuthCodeURL(state, endpoints.UserInfoURL(a.Domain)), http.StatusTemporaryRedirect)
 	}
 }
 
-func RenderFileFunc(name string, data []byte) http.HandlerFunc {
+func (a *Handler) RenderFile(name string, data []byte) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		bits, err := ioutil.ReadFile(name)
 		if err != nil {
@@ -198,7 +168,7 @@ func RenderFileFunc(name string, data []byte) http.HandlerFunc {
 
 }
 
-func WriteFileFunc(name string) http.HandlerFunc {
+func (a *Handler) WriteFile(name string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		bits, err := ioutil.ReadFile(name)
 		if err != nil {
